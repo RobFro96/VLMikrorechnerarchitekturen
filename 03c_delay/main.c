b34342d66ca2296b82d186891175a3d35d94752f
RobFro96 <robert_fromm@web.de>
2018-12-29 19:43:09 +0100

    Starting Pattern Tutorial-Reihe

---
+++ 03c_delay/main.c
@@ -0,0 +1,22 @@
+#include <msp430.h>
+#include <stdint.h>
+
+// Hauptprogramm
+int main(void) {
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	P1DIR = 0xFF;	// Alle Pin als Ausgänge
+	P1OUT = 0x00;   // Alle Ausgänge auf Low
+
+	while (1) {
+		P1OUT |= BIT0;	// set P1.0
+		P1OUT &= ~BIT1;	// clear P1.1
+
+		__delay_cycles(200000);
+
+		P1OUT &= ~BIT0;	// clear P1.0
+		P1OUT |= BIT1;	// set P1.1
+
+		__delay_cycles(200000);
+	}
+}
